eb518810164ad20b0695f9b00f64da16fa9c857d
wewark <khaled.hamedt@gmail.com>
2021-06-03 01:14:56 +0200

    Change default opacity to 1

--- src/default-settings.js
+++ src/default-settings.js
@@ -2,14 +2,14 @@ const kDefaultSettings = {
   upperBaselinePos: 0.15,
   lowerBaselinePos: 0.85,
   primaryImageScale: 0.75,
-  primaryImageOpacity: 0.85,
+  primaryImageOpacity: 1,
   primaryTextScale: 0.95,
-  primaryTextOpacity: 0.85,
+  primaryTextOpacity: 1,
   secondaryImageScale: 0.5,
-  secondaryImageOpacity: 0.85,
+  secondaryImageOpacity: 1,
   secondaryTextScale: 1.0,
   secondaryTextStroke: 2.0,
-  secondaryTextOpacity: 0.85,
+  secondaryTextOpacity: 1,
 };
 
 module.exports = kDefaultSettings;
